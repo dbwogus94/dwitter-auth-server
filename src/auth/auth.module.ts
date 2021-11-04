6fb965f54fc28107c351e7f5aab82c6b56516edb
jae hyun <1994dbwogus@gmail.com>
2021-11-04 17:21:58 +0900

    feat(controller): user 컨트롤러 제거, auth 컨트롤러 추가
    - 이미 사용중인 dwitter api에 맞춰 컨트롤러 재정의

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UserModule],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
