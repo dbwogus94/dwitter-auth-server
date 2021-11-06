fb8597737626b5728e9539be06455ee392f02ccc
jae hyun <1994dbwogus@gmail.com>
2021-11-06 20:57:56 +0900

    feat(login): Login local 인증 전략 적용
    # auth.controller.ts
    - login 메서드에 LocalAuthGuard적용
    
    #  auth.service.ts
    - validateUser 메서드 정의
    
    # local.strategy.ts 신규 생성
    - PassportStrategy(Strategy)를 상속하는 클래스이다.
    - passport local 전략을 사용한다.
    - LocalAuthGuard가 호출시 직접적으로 사용되는 클래스이다.
    - 요청이 들어오면 기본적으로 인증에 사용되는 필드가 빈값인지 체크한다.
    - 인증에 사용되는 필드가 빈 값이 아니라면 validate 메서드를 호출한다.
    - validate까지 모두 통과되면 다음 로직을 호출한다.(interceptor -> pipe -> 라우터)
    
    # local-auth.guard.ts
    - AuthGuard를 상속하는 클래스 LocalAuthGuard이다.
    - 데코레이터로 사용이 가능하다.
    - LocalAuthGuard는 내부적으로 LocalStrategy을 사용한다.
    
    # auth.module.ts
    - Passport를 사용하기 위한 의존성 주입
    
    # login.dto.ts 정의
    - authController.login()에 사용되는 dto이다.
    - ValidationPipe는 class-validator의 데코레이터를 사용하여 유효성 검증을 한다.
    - 하지만 pipe는 guard보다 늦게 호출된다.
    - guard는 인증에 관련된 필드가 빈값이라면 401에러를 내보낸다.
    - 그렇기 때문에 LoginDto에서는 @IsNotEmpty 검증을 수행할 필요가 없다.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,29 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Post,
-  Redirect,
-  Res,
-} from '@nestjs/common';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
-import { Response } from 'express';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
-  async signup(@Body() body: SignupDto, @Res() res: Response) {
-    await this.authService.signup(body);
+  async signup(@Body() signupDto: SignupDto, @Res() res: Response) {
+    await this.authService.signup(signupDto);
     return res.redirect(308, '/auth/login');
     // 308: GET이 아닌 링크/동작을 지닌, 웹 사이트의 재편성.
     // 301: GET 방식에 대한 웹 사이트의 재편성.
   }
 
+  // Guard는 빈값 체크를 알아서 한다. 빈값이면 401에러를 내보낸다.
+  // Guard는 Pipe보다 먼저 실행된다,
+  // 즉, 가드를 사용하면 ValidationPipe를 사용한 빈값 체크를 사용할 수 없다
+  @UseGuards(LocalAuthGuard)
   @Post('/login')
-  login() {
-    //this.authService.login(body);
+  async login(@Body() loginDto: LoginDto) {
+    this.authService.login(loginDto);
     return { text: '로그인' };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,4 +39,27 @@ export class AuthService {
   }
 
   async login(loginDto: LoginDto): Promise<any> {}
+
+  async validateUser(username: string, pass: string): Promise<object | null> {
+    const user = await this.userService.findByUsername(username);
+    if (!user) {
+      return null;
+      /* 
+        Q) UnauthorizedException()  VS  NotFoundException() 어떤것이 맞을까?
+        A) http 응답 규칙에 따르면 등록된 유저(자원)가 없기 때문에 404 NotFoundException을 내보내야 한다.
+          하지만 인증 로직은 특성상(보안) 모호하게 전달해야 한다. 
+          때문에 가입된 유저가 없는 것도, 인증에 실패하는 것도 동일하게 401로 처리한다.
+          => 리턴이 null인 경우 local-strategy.ts에서 공통으로 throw new UnauthorizedException()처리한다.
+      */
+    }
+
+    const isEqual = await bcrypt.compare(pass, user.password);
+    if (isEqual) {
+      // Object Destructuring 기법
+      const { password, ...result } = user;
+      return result;
+      // result에는 password를 제외하고 담긴다.
+    }
+    return null;
+  }
 }
