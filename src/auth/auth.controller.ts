4d35bbd1c49d169a30eba3565b8a8912e39c3521
jae hyun <1994dbwogus@gmail.com>
2021-11-14 20:04:18 +0900

    refactor(AuthController): AccessToken을 확인 로직 이동
    # AuthService로 AccessToken 확인 로직 이동

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import {
   ApiBearerAuth,
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiHeader,
   ApiNoContentResponse,
   ApiOkResponse,
   ApiTags,
@@ -33,9 +34,11 @@ export class AuthController {
     // 301: GET 방식에 대한 웹 사이트의 재편성.
   }
 
-  // Guard는 빈값 체크를 한다. 빈값이면 401에러를 내보낸다.
-  // Guard는 Pipe보다 먼저 실행된다,
-  // 즉, 가드를 사용하면 ValidationPipe를 사용한 빈값 체크를 사용할 수 없다
+  // local 전략을 사용하는 Guard는
+  // local 전략에 사용되는 필드(username, password)에 한에서 빈값 체크를 한다.
+  //  => 전략에 사용되는 필드는 LocalStrategy의 생성자 super를 통해 설정된다.
+  // 그리고 Guard는 Pipe보다 먼저 실행된다,
+  // 즉, 가드를 사용하면 필수 필드에 대해서는 ValidationPipe를 사용한 빈값 체크를 사용할 수 없다.
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   @ApiCreatedResponse({ description: responseMessage.login[201] })
@@ -47,28 +50,28 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Get('/me')
   @ApiCreatedResponse({ description: responseMessage.me[200] })
-  @ApiUnauthorizedResponse({ description: errorMessage.me[401] })
+  @ApiUnauthorizedResponse({ description: errorMessage.me['default'] })
   me(@Req() req, @Headers('authorization') token: string): object {
-    const { username } = req.user; // JwtAuthGuard에서 생성
-    return { username, accessToken: this.authService.getAccessToken(token) };
+    const { username, accessToken } = req.user; // JwtAuthGuard.handleRequest에서 생성
+    return { username, accessToken };
   }
 
+  //@UseGuards(JwtAuthGuard)를 사용할 수 없음
+  // => refresh 작업은 access토큰이 만료여도 가드를 통과해야 하기 때문이다.
   @Get('/refresh')
   @ApiOkResponse({ description: responseMessage.refresh[200] })
-  @ApiUnauthorizedResponse({ description: errorMessage.refresh[401] })
+  @ApiUnauthorizedResponse({ description: errorMessage.refresh['default'] })
   async refresh(@Query() refreshDto: RefreshDto, @Headers('authorization') token: string): Promise<any> {
-    const accessToken: string = this.authService.getAccessToken(token);
-    return this.authService.refresh(refreshDto.username, accessToken);
+    return this.authService.refresh(refreshDto.username, token);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('/logout')
   @HttpCode(204)
   @ApiNoContentResponse({ description: responseMessage.logout[204] })
-  @ApiUnauthorizedResponse({ description: errorMessage.logout[401] })
+  @ApiUnauthorizedResponse({ description: errorMessage.logout['default'] })
   async logout(@Req() req, @Headers('authorization') token: string): Promise<void> {
-    const { id } = req.user;
-    const accessToken: string = this.authService.getAccessToken(token);
+    const { id, accessToken } = req.user;
     await this.authService.logout(id, accessToken);
   }
 }
